e7639a3597d103e7ae8975dc8214ff6dde17e0bb
alessanderleite <alessander.logistica@gmail.com>
2019-02-08 09:41:38 -0200

    declare the Bitmap object, instantiate them inside GameView constructor, finally add the onDraw method

--- app/src/main/java/br/com/alessanderleite/theflyingbird/GameView.java
+++ app/src/main/java/br/com/alessanderleite/theflyingbird/GameView.java
@@ -1,4 +1,25 @@
 package br.com.alessanderleite.theflyingbird;
 
-public class GameView {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class GameView extends View {
+
+    //Bird
+    private Bitmap bird;
+
+    public GameView(Context context) {
+        super(context);
+
+        bird = BitmapFactory.decodeResource(getResources(), R.drawable.bird1);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        canvas.drawBitmap(bird,0,0,null);
+    }
 }
